6f22c6d9352067d6ce78ac08b6b14b40977e8919
Cedric Geissmann <cedric.geissmann@gmail.com>
2022-12-15 08:52:44 +0100

    Added pixelation

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const canvas = document.querySelector("#canvas")
 const ctx = canvas.getContext("2d")
+ctx.imageSmoothingEnabled = false
 const img = document.querySelector("#character")
 
 let x = 0
